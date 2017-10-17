86d4b403023e8cf0f264818132fa9bcc30f7c87b
PeterLyshak <lishak09@gmail.com>
2017-10-17 23:54:32 +0300

    фів

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
 
 	
-if ($(window).width() > 1199) {
-	$('.tooltip').tooltipster({
-		trigger: 'hover',
-		maxWidth: 340,
-		delay: 30,
-		delayTouch: 30
-	});
-} else {
-	$('.tooltip').tooltipster({
-		position: 'bottom',
+$('.tooltip').tooltipster({
+		position: 'top',
 		trigger:"custom",
 		triggerOpen: {
 			click: true,
@@ -22,7 +14,43 @@ if ($(window).width() > 1199) {
 		maxWidth: 280,
 		delay: 30,
 		delayTouch: 30,
-	});
-}
+});
 	
+var vehicleBlock = $('#vehicle-block');
+var maxRangeVehicleScroll = 50;
+var maxRangeVehicleScrollMobile = 400;
+
+$(window).scroll(function(event){
+	var scrollPos = $(this).scrollTop();
+	
+	if ($(window).width() > 1199) {
+		var percentageScroll = ((scrollPos * 100) / maxRangeVehicleScroll);
+	} else {
+		var percentageScroll = ((scrollPos * 100) / maxRangeVehicleScrollMobile);
+	}
+
+	$(vehicleBlock).css('transform', 'translateX(' + percentageScroll + 'px)');
+});
+
+$('[data-toggle="slide-message"]').click(function(e) {
+	e.preventDefault();
+	
+	$(this).prev().addClass('active');
+});
+
+$('#trust').waypoint(function() {
+	$('#trust').addClass('scrolled');
+});
+
+
+$('#reliably').waypoint(function() {
+	$('#reliably').addClass('scrolled');
+}, {
+	offset: '200px'
+});
+
+$("[type = tel]").mask("+7 (999) 999 – 99 – 99");
+
+
+
 
